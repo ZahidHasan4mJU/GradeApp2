dbafbb65822384bc9febb06a676c86856108b245
ZahidHasan4mJU <responsible.zahid@yahoo.com>
2014-10-04 11:03:23 +0600

    succeed

---
+++ GradeApp/GradeApp/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GradeApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Grade aGrade;
+        private void marksShowButton_Click(object sender, EventArgs e)
+        {
+            aGrade = new Grade(Convert.ToDouble(marksPhysicaTextBox.Text), Convert.ToDouble(marksChemistryTextBox.Text),
+                               Convert.ToDouble(marksMathTextBox.Text));
+            marksAverageTextBox.Text = aGrade.getAverage().ToString();
+            marksGradeLetterTextBox.Text = aGrade.getCheck();
+
+        }
+    }
+}
---
+++ GradeApp/GradeApp/Grade.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeApp
+{
+    class Grade
+    {
+        private double physics;
+        private double chemistry;
+        private double mathematics;
+
+        public Grade(double physics, double chemistry, double mathematics)
+        {
+            GetPhysics = physics;
+            GetChemistry = chemistry;
+            Getmathematics = mathematics;
+        }
+        public double GetPhysics
+        {
+            set
+            {
+                physics = value;
+            }
+            get
+            {
+                return physics;
+            }
+        }
+        public double GetChemistry
+        {
+            set
+            {
+                chemistry = value;
+            }
+            get
+            {
+                return chemistry;
+            }
+        }
+        public double Getmathematics
+        {
+            set
+            {
+                mathematics = value;
+            }
+            get
+            {
+                return mathematics;
+            }
+        }
+
+        public double getAverage()
+        {
+            double average = (GetChemistry + GetPhysics + Getmathematics)/3;
+            return average;
+        }
+
+        public string getCheck()
+        {
+            string g = "";
+            if(getAverage() >= 80)
+            {
+                
+                g +="A+";
+            }
+            else if(getAverage() >= 70)
+            {
+                g += "B+";
+            }
+            else if (getAverage() >= 60)
+            {
+                g += "C+";
+            }
+            else if (getAverage() >= 50)
+            {
+                g += "D+";
+            }
+            else
+            {
+                g += "F";
+            }
+
+            
+            
+            return g;
+
+        }
+    }
+}
